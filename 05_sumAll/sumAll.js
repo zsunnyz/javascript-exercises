28bc6b97b3d549ce6f1bd09e853c623947d7cfcf
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-03 11:57:17 +1000

    completed sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof(num1) != "number" || typeof(num2) != "number"){
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    for (let i = (num1 < num2 ? num1 : num2); i <= (num1 < num2 ? num2 : num1); i ++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
