c25ea904d700e357e4269be33caaac337757c7fd
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-03 12:11:51 +1000

    completed leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof(year) != "number"){
+        return "ERROR";
+    }
+    else if (year < 0){
+        return "ERROR";
+    }
 
+    if (!(year % 4)) {
+        if (year % 100) {
+            return true;
+        }
+        else if (year % 400) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
