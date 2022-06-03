b4bfbf46cf14f427269cd036520ba24f84e59eed
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-03 10:51:00 +1000

    completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    else {
+        let retString = "";
+        for (let i = 0; i < num; i ++){
+            retString += string;
+        }
+        return retString;
+    }
 };
 
 // Do not edit below this line
