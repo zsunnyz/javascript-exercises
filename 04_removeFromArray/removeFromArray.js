a6b23254dbb86ff9e2051eebf54fcfa959849502
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-03 11:12:11 +1000

    completed remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++){
+        arr = arr.filter(num => num !== args[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
