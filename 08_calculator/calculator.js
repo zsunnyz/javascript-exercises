f9f00f86d9217c661736a02bf2468abd65ebb50f
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-11 19:35:27 +1000

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((accumulator, currVal) => accumulator + currVal, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.length ? 
+        array.reduce((accumulator, currVal) => accumulator * currVal)
+        : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b 
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+    if (fact == 0){
+        return 1;
+    }
+    else {
+        let product = fact;
+        for (let i = fact - 1; i > 0; i --){
+            product *= i;
+        }
+        return product;
+    }
 };
 
 // Do not edit below this line
