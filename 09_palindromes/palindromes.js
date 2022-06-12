49c834dd7c7b5cac71d9cc7182be9796e2470101
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-12 13:56:55 +1000

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g,"");
+    return string == string.split("").reverse().join("");
 };
 
 // Do not edit below this line
