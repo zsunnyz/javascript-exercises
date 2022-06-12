130d7c430e61550ec6186678b53080276315568d
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-12 14:02:02 +1000

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) {
+        return "OOPS";
+    }
+    else if (num == 1) {
+        return 1;
+    }
+    else {
+        let prevVal = 1;
+        let currVal = 1;
+        let tempVal = currVal;
+        for (let i = 0; i < num - 2; i ++){
+            tempVal = currVal;
+            currVal = currVal + prevVal;
+            prevVal = tempVal;
+        }
+        return currVal;
+    }
 };
 
 // Do not edit below this line
